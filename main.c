3712215fd9fc4603295461748f6e4b90fa0182f5
leokhachatorians <leokhachatorians@gmail.com>
2016-12-05 01:08:29 -0800

    Keep track of our letters

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 
 typedef struct Data Data;
 struct Data {
-    char letter[MESSAGE_LENGTH];
+    char letter;
     int freq;
     Data *left;
     Data *right;
 };
 
 int main() {
-    int i;
+    int i, total_letters = 0;
     size_t j;
     int counts[26] = {0};
     char sentence[BUFSIZ];
@@ -25,6 +25,7 @@ int main() {
 
     size_t len = strlen(sentence);
 
+    // populate our occurence list
     for (j = 0; j < len; j++) {
         char c = sentence[j];
         if (!isalpha(c)) {
@@ -33,14 +34,31 @@ int main() {
         counts[(int)(tolower(c) - 'a')]++;
     }
 
+    // display how many occurences of occuring letters
     for (i = 0; i < 26; i++) {
-        printf("'%c' has %2d occurences.\n", i + 'a', counts[i]);
+        if (counts[i] > 0) {
+            printf("'%c' has %2d occurences.\n", i + 'a', counts[i]);
+            total_letters++;
+        }
+    }
+    //printf("%d\n", total_letters);
+    Data struct_keeper[total_letters];
+    total_letters = 0;
+
+    // populate our 'struct keeper'
+    for (i = 0; i < 26; i++) {
+        if (counts[i] > 0) {
+            char c = i + 'a';
+            Data _ = {c, counts[i], NULL, NULL};
+            struct_keeper[total_letters++] = _;
+        }
     }
 
+    // sanity check to ensure that we did stuff right
+    for (i = 0; i < total_letters; i++) {
+        printf("%c \t %d\n", struct_keeper[i].letter, struct_keeper[i].freq);
+    }
 
-    //Data test = {{'a', '\0'}, 2, NULL, NULL};
-    //Data test2 = {{'b', '\0'}, 3, NULL, NULL};
-    //test.left = &test2;
 
     //printf("%s\t%s\n", test.letter, test.left->letter);
 

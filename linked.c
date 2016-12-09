394b94579c34961fa3cad2f2e6c75668075ecb54
leokhachatorians <leokhachatorians@gmail.com>
2016-12-09 10:15:29 -0800

    Fixed Insert at End
    With some help at ##c, fixed the cycle.

--- linked.c
+++ linked.c
@@ -19,9 +19,11 @@ void insert(Link **, char, int);
 void insert_at_end(Link **, char, int);
 
 int main() {
-    Link *head = (Link*) malloc(sizeof(Link));
-    insert(&head, 'b', 4);
-    insert_at_end(&head, 'c', 4);
+    //Link *head = (Link*) malloc(sizeof(Link));
+    Link *head = NULL;
+    insert(&head, 'a', 1);
+    insert_at_end(&head, 'b', 2);
+    insert_at_end(&head, 'c', 3);
     printf("%c\t%d\n", head->data->letter, head->data->freq);
     return 0;
 }
@@ -31,14 +33,14 @@ void insert(Link **head, char letter, int freq) {
     Link *new_node = (Link*) malloc(sizeof(Link));
     new_node->data = &d;
     new_node->next = *head;
-    *head = new_node;
+    (*head) = new_node;
 }
 
 void insert_at_end(Link **head, char letter, int freq) {
     Data d = {letter, freq, NULL, NULL};
     Link *new_node = (Link*) malloc(sizeof(Link));
     new_node->data = &d;
-    new_node->next = *head;
+    new_node->next = NULL;
 
     Link *p = *head;
     while (p->next != NULL) {

75b7a863ccb95093cc06edcbf98a2d2bce5f56db
leokhachatorians <leokhachatorians@gmail.com>
2016-12-09 09:25:47 -0800

    Insert at end

--- linked.c
+++ linked.c
@@ -16,10 +16,12 @@ struct Link {
 };
 
 void insert(Link **, char, int);
+void insert_at_end(Link **, char, int);
 
 int main() {
     Link *head = (Link*) malloc(sizeof(Link));
-    insert(&head, 'c', 4);
+    insert(&head, 'b', 4);
+    insert_at_end(&head, 'c', 4);
     printf("%c\t%d\n", head->data->letter, head->data->freq);
     return 0;
 }
@@ -29,8 +31,18 @@ void insert(Link **head, char letter, int freq) {
     Link *new_node = (Link*) malloc(sizeof(Link));
     new_node->data = &d;
     new_node->next = *head;
-    (*head) = new_node;
+    *head = new_node;
 }
 
+void insert_at_end(Link **head, char letter, int freq) {
+    Data d = {letter, freq, NULL, NULL};
+    Link *new_node = (Link*) malloc(sizeof(Link));
+    new_node->data = &d;
+    new_node->next = *head;
 
-
+    Link *p = *head;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    *p = *new_node;
+}

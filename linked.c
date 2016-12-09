afb39bdbc6ed505593ab6970c5d645f236aa8084
leokhachatorians <leokhachatorians@gmail.com>
2016-12-09 13:15:28 -0800

    No longer using pointers to pointers to pointers..

--- linked.c
+++ linked.c
@@ -15,36 +15,54 @@ struct Link {
     Data *data;
 };
 
-void insert(Link **, char, int);
-void insert_at_end(Link **, char, int);
+Link *create_link(char, int);
+Link *insert(Link *, char, int);
+Link *insert_at_end(Link *, char, int);
+void traverse(Link *);
 
 int main() {
-    //Link *head = (Link*) malloc(sizeof(Link));
     Link *head = NULL;
-    insert(&head, 'a', 1);
-    insert_at_end(&head, 'b', 2);
-    insert_at_end(&head, 'c', 3);
-    printf("%c\t%d\n", head->data->letter, head->data->freq);
+ //   head = insert(head, 'a', 1);
+ //   head = insert(head, 'b', 1);
+    head = insert_at_end(head, 'c', 1);
+    traverse(head);
     return 0;
 }
 
-void insert(Link **head, char letter, int freq) {
-    Data d = {letter, freq, NULL, NULL};
+Link *create_link(char letter, int freq) {
+    Data *d = (Data*) malloc(sizeof(Data));
+    d->letter = letter;
+    d->freq = freq;
+
     Link *new_node = (Link*) malloc(sizeof(Link));
-    new_node->data = &d;
-    new_node->next = *head;
-    (*head) = new_node;
+    new_node->data = d;
+    return new_node;
 }
 
-void insert_at_end(Link **head, char letter, int freq) {
-    Data d = {letter, freq, NULL, NULL};
-    Link *new_node = (Link*) malloc(sizeof(Link));
-    new_node->data = &d;
-    new_node->next = NULL;
+Link *insert(Link *head, char letter, int freq) {
+    Link *new_node = create_link(letter, freq);
+    new_node->next = head;
 
-    Link *p = *head;
-    while (p->next != NULL) {
-        p = p->next;
+    return new_node;
+}
+
+Link *insert_at_end(Link *head, char letter, int freq) {
+    if (head == NULL) {
+        return insert(head, letter, freq);
     }
-    *p = *new_node;
+
+    Link *cursor = head;
+    while (cursor->next != NULL) {
+        cursor = cursor->next;
+    }
+
+    cursor->next = create_link(letter, freq);
+    return head;
+}
+
+void traverse(Link *head) {
+    while (head != NULL) {
+        printf("%c\t%d\n", head->data->letter, head->data->freq);
+        head = head->next;
+    }
 }

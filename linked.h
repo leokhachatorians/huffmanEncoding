88b1470bffd505c30900614967d269809c569ccf
leokhachatorians <leokhachatorians@gmail.com>
2016-12-09 23:39:45 -0800

    Refactoring smallest value stuff

--- linked.h
+++ linked.h
@@ -7,6 +7,7 @@ struct Node {
     char letter;
     int freq;
     bool del;
+    bool use;
     Node *left;
     Node *right;
 };
--- main.c
+++ main.c
@@ -33,11 +33,10 @@ int main() {
             unique_letters++;
         }
     }
-    traverse(head);
 
-    while (unique_letters > 1) {
+    while (unique_letters != 1) {
         int first = BUFSIZ, second = BUFSIZ;
-        char c_first = ' ', c_second = ' ';
+        char c_first = '\0', c_second = '\0';
         Link *cursor = head;
         Node *n_first = (Node*) malloc(sizeof(Node));
         Node *n_second = (Node*) malloc(sizeof(Node));
@@ -53,7 +52,8 @@ int main() {
                 c_first = cursor->data->letter;
                 n_first = cursor->data;
             }
-            else if (cursor->data->freq < second && cursor->data->letter != c_first) {
+            //else if (cursor->data->freq < second && cursor->data->letter != c_first) {
+            else if (cursor->data->freq < second) {
                 second = cursor->data->freq;
                 c_second = cursor->data->letter;
                 n_second = cursor->data;
@@ -67,13 +67,18 @@ int main() {
         head = merge_nodes(head, n_first, n_second);
         n_first->del = true;
         n_second->del = true;
-        printf("Count: %d\n\n", n_first->freq + n_second->freq);
+        //printf("Count: %d\n\n", n_first->freq + n_second->freq);
         head = remove_node(head);
         head = remove_node(head);
+        free(n_first);
+        free(n_second);
         traverse(head);
         unique_letters--;
         printf("\n");
     }
+    printf("%d\n", total_letters);
+    traverse(head);
+    
 
     return 0;
 }

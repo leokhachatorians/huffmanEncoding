ec6bfbb2deff84f1c815484322a0dbb9ae3566d6
leokhachatorians <leokhachatorians@gmail.com>
2016-12-12 17:03:23 -0800

    Im lost

---
+++ huffman.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "huffman.h"
+
+Link *create_link(char letter, int freq) {
+    Node *d = (Node*) malloc(sizeof(Node));
+    d->letter = letter;
+    d->freq = freq;
+
+    Link *new_node = (Link*) malloc(sizeof(Link));
+    new_node->data = d;
+    return new_node;
+}
+
+Link *merge_nodes(Link *head, Node *first, Node *second) {
+    Node *d = (Node*) malloc(sizeof(Node));
+    d->left = first;
+    d->right = second;
+    d->freq = first->freq + second->freq;
+
+    Link *new_node = (Link*) malloc(sizeof(Link));
+    new_node->data = d;
+    new_node->next = head;
+    return new_node;
+}
+
+Link *insert(Link *head, char letter, int freq) {
+    Link *new_node = create_link(letter, freq);
+    new_node->next = head;
+
+    return new_node;
+}
+
+Link *insert_at_end(Link *head, char letter, int freq) {
+    if (head == NULL) {
+        return insert(head, letter, freq);
+    }
+
+    Link *cursor = head;
+    while (cursor->next != NULL) {
+        cursor = cursor->next;
+    }
+
+    cursor->next = create_link(letter, freq);
+    return head;
+}
+
+Link *remove_node(Link *head) {
+    if (head == NULL) {
+        return head;
+    }
+
+    if (head->data->del) {
+        Link *found = head;
+        head = found->next;
+        free(found);
+        return head;
+    }
+
+    Link *cursor = head;
+    while (cursor != NULL) {
+        if (cursor->next != NULL && cursor->next->data->del) {
+            break;
+        }
+        cursor = cursor->next;
+    }
+    if (cursor != NULL) {
+        Link *found = cursor->next;
+        cursor->next = found->next;
+        free(found);
+    }
+    return head;
+}
+
+void dive(Node *node, Hashtable **table, char dir, char path[], int pathlen) {
+    if (node == NULL) {
+        return;
+    }
+    path[pathlen] = dir;
+    pathlen++;
+
+    if (node->left == NULL && node->right == NULL) {
+        int num, count;
+        count = 0;
+        for (int i = 0; i < pathlen; i++) {
+            if (path[i] == '1' || path[i] == '0') {
+                count++;
+            }
+        }
+        int num_arr[count];
+        printf("Letter: %c", node->letter);
+        for (int i = 0; i < pathlen; i++) {
+            num = (path[i] - '0');
+            num_arr[i] = num;
+
+            printf("Num:%i Arr:%d\n", num, num_arr[i]);
+        }
+        unsigned int k = 0;
+        for (int i = 0; i < count; i++) {
+            k = 10 * k + num_arr[i];
+        }
+        insert_hash_value(table, node->letter, k);
+        //printf("%d \n", get_hash_value(table, node->letter));
+    }
+    else {
+        dive(node->left, table, '0', path, pathlen);
+        dive(node->right, table, '1', path, pathlen);
+    }
+}
+
+void traverse(Link *head) {
+    while (head != NULL) {
+        printf("%c\t%d\n", head->data->letter, head->data->freq);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+unsigned long hash(char str) {
+    unsigned long hash = 5381;
+    int c = str;
+    hash = ((hash << 5) + hash) + c;
+    return hash % 500;
+}
+
+void insert_hash_value(Hashtable **table, char letter, int encoding) {
+    int bucket = hash(letter);
+    for (int tries = 0; tries < 500; tries++) {
+        if ((*table)->table[bucket].in_use == false) {
+            (*table)->table[bucket].in_use = true;
+            (*table)->table[bucket].value = encoding;
+            //printf("%u", (*table)->table[bucket].value);
+        }
+        bucket = (bucket + 1) % 500;
+    }
+   // printf("Bucket: %i\nEncoding: %3d\nChecking %i\n",
+   //         bucket, encoding, (*table)->table[bucket].value);
+}
+
+int get_hash_value(Hashtable **table, char letter) {
+    unsigned long c = hash(letter);
+    return (*table)->table[c].value;
+}
---
+++ huffman.h
@@ -0,0 +1,45 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+#include <stdbool.h>
+
+typedef struct Node Node;
+struct Node {
+    char letter;
+    int freq;
+    bool del;
+    bool use;
+    Node *left;
+    Node *right;
+};
+
+typedef struct Link Link;
+struct Link {
+    Link *next;
+    Node *data;
+};
+
+typedef struct Bucket Bucket;
+struct Bucket {
+    bool in_use;
+    unsigned int value;
+};
+
+typedef struct Hashtable Hashtable;
+struct Hashtable {
+    Bucket table[500];
+};
+
+Link *create_link(char, int);
+Link *insert(Link *, char, int);
+Link *insert_at_end(Link *, char, int);
+Link *remove_node(Link *);
+Link *merge_nodes(Link *, Node *, Node *);
+
+unsigned long hash(char);
+void insert_hash_value(Hashtable **, char, int);
+int get_hash_value(Hashtable **, char);
+
+void dive(Node *, Hashtable **, char, char [], int);
+void traverse(Link *);
+
+#endif // HUFFMAN_H

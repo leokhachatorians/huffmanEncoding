ce993e4c3b4ce24934fe9fea7b51012f54d9bcfc
Leo Khachatorians <leokhachatorians@gmail.com>
2016-12-31 14:45:21 -0800

    Proper letter encodings
    Encodes proper huffman value, but I the output doesnt take into account
    spaces :O

--- Python/huffman.py
+++ Python/huffman.py
@@ -4,20 +4,46 @@ import array
 import binascii
 import codecs
 import pickle
+import queue
 
 class Node():
-    def __init__(self,  word, freq):
-        self.word = word
+    def __init__(self, letter, freq):
+        self.letter = letter
         self.freq = freq
         self.left = None
         self.right = None
         self.delete = False
 
+    def height(self, node):
+        if node is None:
+            return 0
+        else:
+            left_height = self.height(node.left)
+            right_height = self.height(node.right)
+
+            if left_height > right_height:
+                return left_height + 1
+            else:
+                return right_height + 1
+
+    def traverse_level_order(self, root):
+        h = self.height(root)
+        for i in range(1, h+1):
+            self.print_level(root, i)
+
+    def print_level(self, root, level):
+        if root is None:
+            return
+        if level == 1:
+            print("{} - {}".format(root.letter, root.freq))
+        elif level > 1:
+            self.print_level(root.left, level - 1)
+            self.print_level(root.right, level - 1)
+
 class HuffmanTree():
-    def __init__(self, _input, is_a_file=False):
+    def __init__(self, _input):
         self._input = _input
-        self.is_a_file = is_a_file
-        self.word_dict = {}
+        self.letter_dict = {}
         self._parse_input()
         self.node_list = self._fill_node_list()
         self._create_tree()
@@ -27,20 +53,15 @@ class HuffmanTree():
         self._write_to_disk()
 
     def _write_to_disk(self):
-        #for i in self.encoding:
-        #    print(i,self.encoding[i])
-
         buf = ""
         length = 0
         chunk = 0
         arr = array.array('B')
         with open(self._input, 'r') as f:
             for line in f:
-                #for word in line.split():
-                for word in line.splitlines(True):
-                #for word in re.split('(\W)', line):
-                    word += ' '
-                    buf += str(self.encoding[word])
+                for word in re.findall(r'\S+|\n',line):
+                    for letter in word:
+                        buf += str(self.encoding[letter])
 
         for bit in buf:
             if bit == '1':
@@ -50,11 +71,11 @@ class HuffmanTree():
             length += 1
 
             if length == 8:
-                arr.extend([chunk])
+                arr.append(chunk)
                 chunk, length = 0, 0
 
         if length != 0:
-            arr.extend([chunk << (8-length)])
+            arr.append(chunk << (8-length))
 
         with open('a.bin', 'ab') as f:
             for byte in arr:
@@ -62,41 +83,28 @@ class HuffmanTree():
                 f.write(byte.to_bytes((length + 7) // 8, byteorder='big'))
 
     def _parse_input(self):
-        if self.is_a_file:
-            try:
-                with open(self._input, 'r') as f:
-                    self.word_dict = self._create_word_dict(f, is_file=True)
-            except IOError:
-                print("Unable to read file!")
-                exit(1)
-        else:
-            self.word_dict = self._create_word_dict(self._input)
+        try:
+            with open(self._input, 'r') as f:
+                self.letter_dict = self._create_letter_dict(f)
+        except IOError:
+            print("Unable to read file!")
+            exit(1)
 
-    def _create_word_dict(self, contents, is_file=False):
-        word_dict = {}
-        if is_file:
-            for line in contents:
-                for word in line.splitlines(True):
-                #for word in line.split():
-                #for word in re.split('(\W)', line):
-                    word += ' '
-                    if word not in word_dict:
-                        word_dict[word] = 1
+    def _create_letter_dict(self, contents):
+        letter_dict = {}
+        for line in contents:
+            for word in re.findall(r'\S+|\n',line):
+                for letter in word:
+                    if letter not in letter_dict:
+                        letter_dict[letter] = 1
                     else:
-                        word_dict[word] += 1
-        else:
-            for word in contents.split():
-                word += ' '
-                if word not in word_dict:
-                    word_dict[word] = 1
-                else:
-                    word_dict[word] += 1
-        return word_dict
+                        letter_dict[letter] += 1
+        return letter_dict
 
     def _fill_node_list(self):
         node_list = []
-        for word in self.word_dict:
-            node = Node(word, self.word_dict[word])
+        for letter in self.letter_dict:
+            node = Node(letter, self.letter_dict[letter])
             node_list.append(node)
         return node_list
 
@@ -106,7 +114,7 @@ class HuffmanTree():
             n_1 = None
             n_2 = None
 
-            first = second = 100000
+            first = second = 100000000
             for i in range(0, len(self.node_list)):
                 if self.node_list[i].freq < first:
                     second = first
@@ -120,7 +128,7 @@ class HuffmanTree():
             root_node = Node('', n_1.freq + n_2.freq)
             root_node.left = n_1
             root_node.right = n_2
-            self.node_list.insert(0,root_node)
+            self.node_list.append(root_node)
 
             n_1.delete = n_2.delete = True
 
@@ -131,7 +139,7 @@ class HuffmanTree():
             num_entries -= 1
 
     def _encode_tree(self):
-        path = ['' for i in range(1000)]
+        path = ['' for i in range(100)]
         encode_dict = {}
         self._encode(self.root, encode_dict, '', path, 0)
 
@@ -141,8 +149,8 @@ class HuffmanTree():
         path[pathlen] = direction
         pathlen += 1
 
-        if (node.left == None and node.right == None):
-            self.encoding[node.word] = ''.join(path)
+        if node.left is None and node.right is None:
+            self.encoding[node.letter] = ''.join(path[1:pathlen])
         else:
             self._encode(node.left, encode_dict, '0', path, pathlen)
             self._encode(node.right, encode_dict, '1', path, pathlen)
@@ -150,8 +158,8 @@ class HuffmanTree():
 class Decoder():
     def __init__(self, encoding):
         self.encoding = {v:k for k,v in encoding.items()}
-        #with open('tst.pickle', 'wb') as h:
-        #    pickle.dump(self.encoding, h, protocol=pickle.HIGHEST_PROTOCOL)
+        with open('tst3.pickle', 'wb') as h:
+            pickle.dump(self.encoding, h, protocol=pickle.HIGHEST_PROTOCOL)
 
     def _read(self):
         with open('a.bin', 'rb') as f:
@@ -184,9 +192,6 @@ class Decoder():
             print(i, ' ', self.encoding[i])
 
 
-
-
-
 if __name__ == '__main__':
     #sentence = "this is a sentence, this is not a paragraph. this is a sentence! so don't get it twisted ya hear? because other was this sentence would not be a sentence but a sentence would become a paragrah, which is pretty weird if you ask me if if if if if if if if"
 
@@ -194,9 +199,9 @@ if __name__ == '__main__':
     #print()
     #t._preorder_traverse(t.root)
     #t._encode_tree()
-    t = HuffmanTree("book2", True)
-    #print(t.word_dict)
-    t._encode_tree()
+    t = HuffmanTree("od.txt")
+    #t._encode_tree()
 
     e = Decoder(t.encoding)
+    e._check_encoding()
     e._read()
